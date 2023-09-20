9de29cd74978da7faf67a3c4a339de8365f21d92
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-09-20 17:01:56 +0200

    dev: isAdding

--- front/src/app/stock/add/add.component.ts
+++ front/src/app/stock/add/add.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
-import { delay, of, switchMap } from 'rxjs';
+import { faPlus, faCircleNotch } from '@fortawesome/free-solid-svg-icons';
+import { delay, finalize, of, switchMap, tap } from 'rxjs';
 import { NewArticle } from 'src/app/interfaces/article';
 import { ArticleService } from 'src/app/services/article.service';
 
@@ -13,6 +13,7 @@ import { ArticleService } from 'src/app/services/article.service';
 })
 export class AddComponent {
   faPlus = faPlus;
+  faCircleNotch = faCircleNotch;
 
   f = new FormGroup({
     name: new FormControl('Truc', [
@@ -23,6 +24,8 @@ export class AddComponent {
     qty: new FormControl(0, [Validators.required]),
   });
 
+  isAdding = false;
+
   constructor(public articleService: ArticleService, public router: Router) {}
 
   submit() {
@@ -30,10 +33,16 @@ export class AddComponent {
     const newArticle: NewArticle = this.f.value as NewArticle;
     of(undefined)
       .pipe(
+        tap(() => {
+          this.isAdding = true;
+        }),
         delay(1000),
         switchMap(() => this.articleService.addArticle(newArticle)),
         switchMap(() => this.articleService.getArticles()),
-        switchMap(() => this.router.navigateByUrl('/stock'))
+        switchMap(() => this.router.navigateByUrl('/stock')),
+        finalize(() => {
+          this.isAdding = false;
+        })
       )
       .subscribe();
   }
