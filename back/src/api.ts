ebd07001b08e4fd08f02f4ba71f52e0051b1e71c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-09-20 09:56:35 +0200

    dev: added articles api

--- back/src/api.ts
+++ back/src/api.ts
@@ -1,9 +1,14 @@
 import express from "express";
 
+const articles = [
+  { id: "a1", name: "Tournevis", price: 2.99, qty: 150 },
+  { id: "a2", name: "Pelle", price: 4, qty: 25 },
+];
+
 const app = express.Router();
 
 app.get("/articles", (req, res) => {
-  res.json([]);
+  res.json(articles);
 });
 
 export default app;
